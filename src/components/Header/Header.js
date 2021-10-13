d5c0a81c723cab33d414e344aeb444dd1d006b16
Sabrina Araujo <sabrinaaraujo.ds@gmail.com>
2021-10-13 14:27:04 -0300

    teste login, funcao logout, responsividade tablet menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import './Header.css'
 
-function Header({
-    products
-}) {
+function Header() {
+    const history = useHistory();
+
+    const handleClick = () => {
+        localStorage.clear()
+        history.push('/')      
+    }
+
     return (
         <nav className='nav-bar'>
             <div className='logo-header'>
-                <img alt=''></img>
             </div>
             <div className='links-hall'>
                 <a href='/Hall'>Salão</a>
                 <a href='/Kitchen'>Cozinha</a>
                 <a href='/OrderReady'>Pedidos</a>
+                <i onClick={handleClick} className="fas fa-sign-out-alt"></i>                
             </div>
         </nav>
     );
